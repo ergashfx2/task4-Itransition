801ae6c0e1cf658a946501644edaedd443ce7046
ergashfx2 <ergashaliabduvohidov@gmail.com>
2024-02-20 12:15:23 +0500

    commit

--- static/script.js
+++ static/script.js
@@ -1,101 +1,82 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-    var registrationForm = document.getElementById('registrationForm');
+    const registrationForm = document.getElementById('registrationForm');
+    const buttons = document.querySelectorAll('.button2');
+    const deleteButtons = document.querySelectorAll('.delete-button');
+    const logoutButton = document.getElementById('logout');
+    const loginForm = document.getElementById('login-form');
 
     if (registrationForm) {
-      registrationForm.addEventListener('submit', handleRegisterFormSubmit);
+        registrationForm.addEventListener('submit', handleRegisterFormSubmit);
     } else {
-      console.error('Registration form not found');
+        console.error('Registration form not found');
     }
-  var buttons = document.querySelectorAll('.button2');
-  buttons.forEach(function(button) {
-    button.addEventListener('click', function() {
-        Block_user(this);
-    });
-});
 
-var deleteButtons = document.querySelectorAll('.delete-button');
-deleteButtons.forEach(function(delete_button) {
-  delete_button.addEventListener('click', function() {
-      DeleteUser(this);
-  });
-});
-
-  var logoutButton = document.getElementById('logout');
-  if (logoutButton) {
-      logoutButton.addEventListener('click', handleLogout);
-  } else {
-      console.error('Logout button not found');
-  }
-
-  var loginForm = document.getElementById('login-form');
-  if (loginForm) {
-      loginForm.addEventListener('submit', handleLoginFormSubmit);
-  } else {
-      console.error('Login form not found');
-  }
-});
-
-
-function DeleteUser(Button){
-    console.log("Clicked delete")
-    var button = Button;
-    var email = button.id;
-    var formData = new FormData();
-    formData.append('email',email);
-    fetch('/delete_user',{
-    method : 'POST',
-    body: formData
-
-
-})
-.then(response=>{
-    if (response.ok){
-        alert("Deleted Successfully")
-        location.reload();
-    }else{
-        alert("Failed please try again")
-    }
-})
-}
-
-
-function Block_user(clickedButton) {
-    console.log("clicked");
-    var button = clickedButton;
-    var email = button.id;
-    var formData = new FormData();
-    var icon = button.querySelector('i');
-    formData.append('email', email);
-    if (icon.classList.contains('bi-unlock-fill')) {
-        fetch('/unblock_user', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => {
-            if (response.ok) {
-                alert('Unblocked successfully');
-                icon.remove();
-                button.innerHTML += '<i class="bi bi-lock-fill bi-7x"></i>';
-                location.reload();
-            } else {
-                alert('Failed. Please try again.');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            blockUser(button);
         });
-}else {
-    fetch('/block_user', {
+    });
+
+    deleteButtons.forEach(deleteButton => {
+        deleteButton.addEventListener('click', () => {
+            deleteUser(deleteButton);
+        });
+    });
+
+    if (logoutButton) {
+        logoutButton.addEventListener('click', handleLogout);
+    } else {
+        console.error('Logout button not found');
+    }
+
+    if (loginForm) {
+        loginForm.addEventListener('submit', handleLoginFormSubmit);
+    } else {
+        console.error('Login form not found');
+    }
+});
+
+function deleteUser(button) {
+    console.log("Clicked delete");
+    const email = button.id;
+    const formData = new FormData();
+    formData.append('email', email);
+
+    fetch('/delete_user', {
         method: 'POST',
         body: formData
     })
     .then(response => {
         if (response.ok) {
-            alert('Blocked successfully');
-                icon.remove();
-                button.innerHTML += '<i class="bi bi-unlock-fill"></i>';
-                location.reload();
+            alert("Deleted Successfully");
+            location.reload();
+        } else {
+            alert("Failed please try again");
+        }
+    });
+}
+
+function blockUser(clickedButton) {
+    console.log("Clicked block");
+    const button = clickedButton;
+    const email = button.id;
+    const formData = new FormData();
+    const icon = button.querySelector('i');
+    formData.append('email', email);
+
+    const url = icon.classList.contains('bi-unlock-fill') ? '/unblock_user' : '/block_user';
+
+    fetch(url, {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => {
+        if (response.ok) {
+            const message = url === '/unblock_user' ? 'Unblocked successfully' : 'Blocked successfully';
+            alert(message);
+            icon.remove();
+            button.innerHTML += `<i class="bi bi-${url === '/unblock_user' ? 'lock' : 'unlock'}-fill"></i>`;
+            location.reload();
         } else {
             alert('Failed. Please try again.');
         }
@@ -105,49 +86,45 @@ function Block_user(clickedButton) {
     });
 }
 
-}
-
-
 function handleLogout() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', '/logout');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onload = function() {
-      if (xhr.status === 200) {
-          window.location.href = '/';
-      } else {
-          console.error('Logout failed');
-      }
-  };
-  xhr.send();
+    fetch('/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            window.location.href = '/';
+        } else {
+            console.error('Logout failed');
+        }
+    });
 }
 
 function handleLoginFormSubmit(event) {
-  event.preventDefault();
+    event.preventDefault();
+    const formData = new FormData(this);
 
-  const formData = new FormData(this);
-
-  fetch('/login-user', {
-      method: 'POST',
-      body: formData
-  })
-  .then(response => {
-      if (response.ok) {
-          alert('Login was successful!');
-          window.location.href = '/';
-      } else {
-          alert('Login failed. Please try again.');
-      }
-  })
-  .catch(error => {
-      console.error('Error:', error);
-  });
+    fetch('/login-user', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('Login was successful!');
+            window.location.href = '/';
+        } else {
+            alert('Login failed. Please try again.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
-
 
 function handleRegisterFormSubmit(event) {
     event.preventDefault();
-
     const formData = new FormData(this);
 
     fetch('/create-user', {
@@ -165,4 +142,4 @@ function handleRegisterFormSubmit(event) {
     .catch(error => {
         console.error('Error:', error);
     });
-  }
+}
